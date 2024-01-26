1e63f433572ab002d9cc132145e1d663fb7493ba
Nakhla Rafi <nakhla054@gmail.com>
2024-01-26 01:31:43 -0500

    change

--- Code/Default/run.py
+++ Code/Default/run.py
@@ -163,7 +163,7 @@ def train(t = 5, p='Math'):
                     bans = lst
                     maxl = score
                     print("find better score " + str(score) + "," + str(score2))
-                    save_model(model)
+                    save_model(model,p)
                     #torch.save(model.state_dict(), os.path.join(wandb.run.dir, 'model.pt'))
                 model = model.train()
             for i in range(len(dBatch)):
