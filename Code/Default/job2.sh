f534c58273ed50660f078b5e0ec13d9e3debfcc0
Nakhla Rafi <nakhla054@gmail.com>
2024-01-26 01:36:42 -0500

    job 2 for sbatch

---
+++ Code/Default/job2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J Test-GPUs --mem=100M --gpus=4 -w virya4
+source /etc/profile.d/modules.sh 
+module load anaconda/3.2023.03
+python runtotal.py Cli
+python runtotal.py Codec
+python runtotal.py Compress
+python runtotal.py Csv
+python runtotal.py JacksonCore
+python runtotal.py JacksonXml
+python runtotal.py Jsoup
+python runtotal.py Lang
+python runtotal.py Math
+python runtotal.py Mockito
+python runtotal.py Time
+# python runtotal.py Collections
